6cec6c51d82f7e928b6aebb46c6b0157c808c118
ar.papeta <ar.papeta@gmail.com>
2018-11-01 21:09:51 +0200

    Add home controller and View

--- WebApplicationMVC_TEST_APP/Controllers/HomeController.cs
+++ WebApplicationMVC_TEST_APP/Controllers/HomeController.cs
@@ -3,28 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplicationMVC_TEST_APP.Models;
 
 namespace WebApplicationMVC_TEST_APP.Controllers
 {
     public class HomeController : Controller
     {
+        BookContext db = new BookContext();
         public ActionResult Index()
         {
+            var books = db.Books;
+            ViewBag.Books = books;
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
     }
 }
